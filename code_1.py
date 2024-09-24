70f4679004bd85b80bc1378d5fe3c163f3ecae12
EpicOnionBoi <103018129+EpicOnionBoi@users.noreply.github.com>
2024-09-24 08:08:47 +0000

    Added function to count the frequencies of pairs in the names file, and also added part that writes the frequencies of the pairs into a text file.

--- code_1.py
+++ code_1.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 with open('names.txt', 'r') as f:
     names = f.readlines()
     names = [name.strip() for name in names]
@@ -16,3 +17,21 @@ def letterpairs(name):
         pairs.append([name[e], name[e+1]])
     print(pairs)
 letterpairs(name)
+
+def countpairs(names):
+    noendpairs = defaultdict(int)
+    startendpairs = defaultdict(int)
+    for name in names:
+        startendpairs[('#', name[0])] += 1
+        for e in range(len(name) - 1):
+            pair = (name[e], name[e+1])
+            noendpairs[pair] += 1
+        startendpairs[(name[-1], '$')] += 1
+    return noendpairs, startendpairs
+
+pairlist = countpairs(names)
+with open('pair_freqs_raw.txt', 'w') as f:
+    for pair, number in pairlist[0].items():
+        f.write(f"({pair}, {number})\n")
+    for pair, number in pairlist[1].items():
+        f.write(f"({pair}, {number})\n")
