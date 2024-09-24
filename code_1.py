bfb07783c737edf5ff3d271544ef9bc2f32de877
EpicOnionBoi <103018129+EpicOnionBoi@users.noreply.github.com>
2024-09-24 07:40:15 +0000

    Opened names file and added function for getting the number of names, and longest and shortest names.

---
+++ code_1.py
@@ -0,0 +1,11 @@
+with open('names.txt', 'r') as f:
+    names = f.readlines()
+    names = [name.strip() for name in names]
+
+def countshortlongnames(names):
+    number = len(names)
+    shortest = min(names, key=len)
+    longest = max(names, key=len)
+    return number, shortest, longest
+
+print(countshortlongnames(names))
