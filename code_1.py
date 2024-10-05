8c3aefca014ee3db6552abb1bbe8afedca2cb0ca
EpicOnionBoi <103018129+EpicOnionBoi@users.noreply.github.com>
2024-10-05 11:44:44 +0000

    Code can now demonstrate correctness for coin and spinner as I just made a function that runs either the coin or the spinner 100 times and puts the results into a dictionary.

--- code_1.py
+++ code_1.py
@@ -43,6 +43,13 @@ def chosenletterpairs(letter, pairs):
 with open('pair_freqs_raw.txt', 'r') as file:
         pairlist = [ast.literal_eval(line.strip()) for line in file.readlines()]
 
+def randomchoice(thing):
+    results = defaultdict(int)
+    for e in range(100):
+        result = random.choice(thing)
+        results[result]+=1
+    return results
+
 print("Welcome to the Tiny Language Model\nUse the menu below to use the Tiny Language Model\n(1) Basic statistics (number of names, shortest, longest, etc)\n(2) Split a name into letter pairs\n(3) Display the first _ lines of the sorted pairs frequency table\n(4) Display pairs starting with a particular character\n(5) Flip the coin and demonstrate correctness\n(6) Spin the numbered wheel and demonstrate correctness\n(7) Generate _ new names starting with letter _\n(8) Generate _ random names\n(9) Demonstrate the result of an untrained character-pair freq. table\n(10) Evaluate a name against the model by printing its pair probabilities")
 option = input("Enter 1 to 10, or 0 to quit: ")
 if option == "0":
@@ -62,6 +69,8 @@ elif option == "4":
 elif option == "5":
     coin = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
     print(random.choice(coin))
+    print(randomchoice(coin))
 elif option == "6":
     spinner = [0, 0, 1, 2, 3, 3, 3, 3, 3, 3]
     print(random.choice(spinner))
+    print(randomchoice(spinner))
